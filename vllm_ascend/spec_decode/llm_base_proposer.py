d8063c902df6987eccdbcafb78a0c86d7cc7f29a
zhangsicheng5 <zhangsicheng5@huawei.com>
2026-09-20 20:33:24 +0800

    short sequence also use nano op
    Signed-off-by: zhangsicheng5 <zhangsicheng5@huawei.com>

--- vllm_ascend/spec_decode/llm_base_proposer.py
+++ vllm_ascend/spec_decode/llm_base_proposer.py
@@ -798,7 +798,6 @@ class AscendSpecDecodeBaseProposer(SpecDecodeBaseProposer):
                         if self.runner._offload_pool_generations is not None
                         else None
                     ),
-                    nano_eligible=True,
                     offload_dummy=True,
                     req_ids_tensor=req_ids_tensor,
                     token_to_req=token_to_req,
@@ -2341,7 +2340,6 @@ class AscendSpecDecodeBaseProposer(SpecDecodeBaseProposer):
             group_key_cache_idx=common_attn_metadata.group_key_cache_idx,
             req_topk_buffer_slots=common_attn_metadata.req_topk_buffer_slots,
             req_topk_buffer_generations=common_attn_metadata.req_topk_buffer_generations,
-            nano_eligible=common_attn_metadata.nano_eligible,
             offload_dummy=common_attn_metadata.offload_dummy,
             req_ids_tensor=common_attn_metadata.req_ids_tensor,
             token_to_req=token_to_req,
@@ -2442,7 +2440,6 @@ class AscendSpecDecodeBaseProposer(SpecDecodeBaseProposer):
             group_key_cache_idx=common_attn_metadata.group_key_cache_idx,
             req_topk_buffer_slots=common_attn_metadata.req_topk_buffer_slots,
             req_topk_buffer_generations=common_attn_metadata.req_topk_buffer_generations,
-            nano_eligible=common_attn_metadata.nano_eligible,
             offload_dummy=common_attn_metadata.offload_dummy,
             req_ids_tensor=common_attn_metadata.req_ids_tensor,
             token_to_req=common_attn_metadata.token_to_req,
